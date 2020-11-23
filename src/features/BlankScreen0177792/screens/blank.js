3707469199c83259428ff545bde1479f373b9ef9
Crowdbotics <team@crowdbotics.com>
2020-11-23 06:02:10 +0000

    Update components

--- src/features/BlankScreen0177792/screens/blank.js
+++ src/features/BlankScreen0177792/screens/blank.js
@@ -23,7 +23,12 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = {
+    TextInput_18: "",
+    TextInput_20: "",
+    TextInput_22: "",
+    TextInput_24: ""
+  }
 
   render = () => (
     <View>
@@ -32,6 +37,26 @@ export default class Blank extends React.Component {
           uri:
             "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/Screen_Shot_2020-11-22_at_11.39.07_PM.png"
         }}
+      />
+      <TextInput
+        placeholder="EIU NetID"
+        value={this.state.TextInput_18}
+        onChangeText={nextValue => this.setState({ TextInput_18: nextValue })}
+      />
+      <TextInput
+        placeholder="XXXXXXXXX"
+        value={this.state.TextInput_20}
+        onChangeText={nextValue => this.setState({ TextInput_20: nextValue })}
+      />
+      <TextInput
+        placeholder="PASSWORD"
+        value={this.state.TextInput_22}
+        onChangeText={nextValue => this.setState({ TextInput_22: nextValue })}
+      />
+      <TextInput
+        placeholder="XXXXXXXXX"
+        value={this.state.TextInput_24}
+        onChangeText={nextValue => this.setState({ TextInput_24: nextValue })}
       />
     </View>
   )
@@ -42,6 +67,13 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
-  Image_3: {}
+  Image_3: {},
+  View_1: {},
+  Image_3: {},
+  TextInput_18: {},
+  TextInput_20: {},
+  TextInput_22: {},
+  TextInput_24: {}
 })
